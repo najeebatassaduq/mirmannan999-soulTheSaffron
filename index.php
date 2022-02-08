bc59fbd9490706f6d2c5e07b39ab30d7ea34078f
mirmannan99 <mirmannan99@gmail.com>
2022-02-08 21:15:12 +0530

    yml file edit

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ require"navbar.php";
                     <!-- Banner Single Carousel Start -->
                     <div class="single-carousel-wrap home_6_slide_1">
                         <div class="banner-caption text-center text-lg-left">
-                            <h4>Rubby Store</h4>
+                            <h4>Soul The Store</h4>
                             <h2>Ring Solitaire <br> for Princess</h2>
                             <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
                             <a href="#" class="btn-long-arrow">Learn More</a>
